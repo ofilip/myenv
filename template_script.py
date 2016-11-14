f773a734d819fe1d9e3aff7bf7f5a4aeacb7a0b1
Ondrej Filip <ondrej.filip@firma.seznam.cz>
2016-11-14 14:41:36 +0100

    Add python template script

---
+++ template_script.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import sys
+import argparse
+import six
+
+
+def main(topn, switch, filename):
+    print('topn={} switch={} filename={}'.format(topn, switch, filename))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='PLACEHOLDER')
+    parser.add_argument('-n', '--topn', type=int, required=False, default=-1)
+    parser.add_argument('--switch', required=False, default=False, action='store_true')
+    parser.add_argument('filename')
+
+    if len(sys.argv)==1:
+        parser.print_help()
+        sys.exit(1)
+
+    args = parser.parse_args()
+    main(args.topn, args.switch, args.filename)
