ec982ad35d9e0b22dea9e57cb37fc459a55cc5ec
Ondrej Filip <ondrej.filip@firma.seznam.cz>
2018-01-15 13:40:42 +0100

    Add meld as diff/merge tool in git

--- config/git_config/common.sh
+++ config/git_config/common.sh
@@ -1,2 +1,9 @@
 git config --global user.name "Ondrej Filip"
 git config --global push.default simple
+
+git config --global diff.tool meld
+git config --global difftool.meld.cmd "meld \"\$LOCAL\" \"\$REMOTE\""
+git config --global difftool.prompt false
+
+git config --global merge.tool meld
+git config --global mergetool.meld.cmd "meld \"\$LOCAL\" \"\$BASE\" \"\$REMOTE\""
